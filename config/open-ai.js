bf53fe820cd7a2646b8c798559fb711ea30f1591
Gabriel Postigo <gpostigovi@gmail.com>
2024-04-22 10:41:32 +0200

    Remove Open AI Configuration

--- config/open-ai.js
+++
@@ -1,9 +0,0 @@
-import { Configuration, OpenAIApi } from 'openai';
-
-const configuration = new Configuration({
-  apiKey: "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",
-});
-
-const openai = new OpenAIApi(configuration);
-
-export default openai;
